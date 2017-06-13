0ebf30c07f254f8c0df127cff28e2a1d2f0d02b7
Simon <sipdx@outlook.com>
2017-06-13 12:27:19 -0700

    Made a bit more generic.

--- csv2dynamodb.py
+++ csv2dynamodb.py
@@ -1,6 +1,7 @@
 #CSV import format
-# AAPL, Apple
-# AMD, AMD
+#  AAPL, Apple
+#  AMD, AMD
+
 
 # Format for DynamoDB
 # {"id":{"s":"AAPL"},"description":{"s":"Apple"}}
@@ -12,12 +13,13 @@ import csv
 filename = 'data.csv'
 outputfilename = 'dynamodbimport'
 
+def formatStr(name, type, value):
+        return "\"" + name + "\":{\"" + type + "\":\"" + value + "\"}"
+
+
 with open(filename) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-
         outputfile = open(outputfilename, 'w')
 
         for line in reader:
-                print (line)
-                outputfile.write("{\"id\":{\"s\":\"" + line[0] + "\"},\"description\":{\"s\":\""+ line[1] + "\"}}" +  "\n")
-				
+                outputfile.write("{" + formatStr("id", "s", line[0]) + "," + formatStr("description","s", line[1]) +"}" +  "\n")
