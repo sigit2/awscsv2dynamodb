9983782da1bc075dc046d3ea4d6a4deca06ad0c4
sigit2 <sipdx@outlook.com>
2017-06-13 11:24:16 -0700

    Add files via upload

---
+++ csv2dynamodb.py
@@ -0,0 +1,23 @@
+#CSV import format
+#   AAPL, Apple
+
+
+# Format for DynamoDB
+# {"id":{"s":"AAPL"},"description":{"s":"Apple"}}
+# {"id":{"s":"AMD"},"description":{"s":"AMD"}}
+
+
+import csv
+
+filename = 'data.csv'
+outputfilename = 'dynamodbimport'
+
+with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+
+        outputfile = open(outputfilename, 'w')
+
+
+        for line in reader:
+                print (line)
+                outputfile.write("{\"id\":{\"s\":\"" + line[0] + "\"},\"description\":{\"s\":\""+ line[1] + "\"}}" +  "\n")
